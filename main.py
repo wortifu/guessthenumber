7017315d32e4cd8586da192c398959e6de99c39d
Dwi Adi Sugiantoro <66587179+wortifu@users.noreply.github.com>
2024-11-08 17:18:08 +0700

    Create main.py
    add main code

---
+++ main.py
@@ -0,0 +1,31 @@
+import random
+
+print("Hi welcome to the game, This is a number guessing game.\nYou got 7 chances to guess the number. Let's start the game")
+
+number_to_guess = random.randrange(100)
+
+chances = 7
+
+guess_counter = 0
+
+while guess_counter < chances:
+
+    guess_counter += 1
+    my_guess = int(input("""
+    ===============================
+    Guess the number between 1-100
+    ===============================
+    """))
+
+    if my_guess == number_to_guess:
+        print(f'The number is {number_to_guess} and you found it right !! in the {guess_counter} attempt')
+        break
+
+    elif guess_counter >= chances and my_guess != number_to_guess:
+        print(f'Oops sorry, The number is {number_to_guess} better luck next time')
+
+    elif my_guess > number_to_guess:
+        print(f'--^^ Your guess is higher \n guessed : {guess_counter} times')
+
+    elif my_guess < number_to_guess:
+        print(f'--^^ Your guess is lesser \n guessed : {guess_counter} times')
